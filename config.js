a22ab8c8d9fc4e2ab50e11b0456844a0bb6004a8
Stephan van Rooij <github@svrooij.nl>
2017-12-25 12:33:41 +0100

    Some code refactored and according to standardjs

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var pkg = require('./package.json');
+var pkg = require('./package.json')
 var config = require('yargs')
     .usage(pkg.name + ' ' + pkg.version + '\n' + pkg.description + '\n\nUsage: $0 [options]')
     .describe('v', 'possible values: "error", "warn", "info", "debug"')
@@ -6,19 +6,19 @@ var config = require('yargs')
     .describe('u', 'mqtt broker url. See https://github.com/mqttjs/MQTT.js#connect-using-a-url')
     .describe('h', 'show help')
     .alias({
-        'h': 'help',
-        'n': 'name',
-        'u': 'url',
-        'v': 'verbosity'
+      'h': 'help',
+      'n': 'name',
+      'u': 'url',
+      'v': 'verbosity'
     })
     .default({
-        'u': 'mqtt://127.0.0.1',
-        'n': 'sonos',
-        'v': 'info'
+      'u': 'mqtt://127.0.0.1',
+      'n': 'sonos',
+      'v': 'info'
     })
-    //.config('config')
+    // .config('config')
     .version()
     .help('help')
-    .argv;
+    .argv
 
-module.exports = config;
+module.exports = config
