11a0bab4d6eb14c80ed405640423391c454f5935
Stephan van Rooij <github@svrooij.nl>
2018-01-01 17:14:23 +0100

    feat: Verbosity is now a choises parameter

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var pkg = require('../package.json')
 var config = require('yargs')
     .usage(pkg.name + ' ' + pkg.version + '\n' + pkg.description + '\n\nUsage: $0 [options]')
-    .describe('v', 'possible values: "error", "warn", "info", "debug"')
+    .describe('v', 'Verbosity level')
     .describe('n', 'instance name. used as mqtt client id and as prefix for connected topic')
     .describe('u', 'mqtt broker url. See https://github.com/mqttjs/MQTT.js#connect-using-a-url')
     .describe('d', 'Publish distinct track states')
@@ -20,6 +20,8 @@ var config = require('yargs')
       'v': 'info',
       'd': false
     })
+    .choices('v', ['error', 'warn', 'info', 'debug'])
+    .wrap(80)
     // .config('config')
     .version()
     .help('help')
