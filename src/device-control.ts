4c3ad9a5c17936bd08cb80662bd4cace5b29b961
Stephan van Rooij <stephan@svrooij.nl>
2020-06-11 17:00:03 +0200

    fix: Initial version of command reply (#103)
    Fixes #101

--- src/device-control.ts
+++ src/device-control.ts
@@ -1,10 +1,15 @@
 import { SonosCommands } from "./sonos-commands";
 
 export class DeviceControl {
-  public readonly command?: SonosCommands
+  public readonly command?: SonosCommands;
+  public readonly replyTopic?: string;
   constructor(command?: string, public readonly sonosCommand?: string, public readonly input?: any) {
     if(command !== undefined && Object.values(SonosCommands).some(v => v === command.toLowerCase())) {
       this.command = command.toLowerCase() as SonosCommands;
+    }
+
+    if(command === SonosCommands.AdvancedCommand && input.reply) {
+      this.replyTopic = input.reply;
     }
   }
 
